178d0d9c9942d21fa37e8e6a5480189cc09afea2
Hetmarum <kpxmedet@gmail.com>
2025-10-21 19:25:25 +0300

    code review fixes

--- scrape_books/spiders/books_spider.py
+++ scrape_books/spiders/books_spider.py
@@ -2,14 +2,31 @@ import scrapy
 from scrapy.http import Response
 
 
-class BooksSpiderSpider(scrapy.Spider):
+class BookItem(scrapy.Item):
+    title = scrapy.Field()
+    price = scrapy.Field()
+    amount_in_stock = scrapy.Field()
+    rating = scrapy.Field()
+    category = scrapy.Field()
+    description = scrapy.Field()
+    upc = scrapy.Field()
+
+
+class BooksSpider(scrapy.Spider):
     name = "books_spider"
     allowed_domains = ["books.toscrape.com"]
     start_urls = ["https://books.toscrape.com/"]
 
+    RATING_MAP = {
+        "One": 1,
+        "Two": 2,
+        "Three": 3,
+        "Four": 4,
+        "Five": 5,
+    }
+
     def parse(self, response: Response):
-        books = response.css("article.product_pod h3 a::attr(href)").getall()
-        for book_url in books:
+        for book_url in response.css("article.product_pod h3 a::attr(href)").getall():
             yield response.follow(book_url, callback=self.parse_book)
 
         next_page = response.css("li.next a::attr(href)").get()
@@ -17,32 +34,36 @@ class BooksSpiderSpider(scrapy.Spider):
             yield response.follow(next_page, callback=self.parse)
 
     def parse_book(self, response: Response):
-        def extract_with_css(query):
-            return response.css(query).get(default="").strip()
+        item = BookItem()
 
-        title = extract_with_css("div.product_main h1::text")
-        price = extract_with_css("p.price_color::text")
-        amount_in_stock = extract_with_css("p.instock.availability::text")
-        amount_in_stock = amount_in_stock.replace("\n", "").strip()
+        item["title"] = (
+            response.css("div.product_main h1::text").get(default="").strip()
+        )
+        item["price"] = response.css("p.price_color::text").get(default="").strip()
 
-        rating_class = response.css("p.star-rating::attr(class)").get()
-        rating = (
-            rating_class.replace("star-rating", "").strip() if rating_class else None
+        stock_text = response.css("p.instock.availability::text").getall()
+        stock_clean = [t.strip() for t in stock_text if t.strip()]
+        item["amount_in_stock"] = (
+            int(response.css("p.instock.availability").re_first(r"\d+"))
+            if stock_clean
+            else 0
         )
 
-        category = response.css("ul.breadcrumb li a::text").getall()[-1].strip()
+        rating_class = response.css("p.star-rating::attr(class)").get(default="")
+        rating_word = rating_class.replace("star-rating", "").strip()
+        item["rating"] = self.RATING_MAP.get(rating_word, None)
 
-        description = response.css("#product_description ~ p::text").get()
-        description = description.strip() if description else ""
+        item["category"] = (
+            response.css("ul.breadcrumb li:nth-child(3) a::text")
+            .get(default="")
+            .strip()
+        )
 
-        upc = response.xpath('//th[text()="UPC"]/following-sibling::td/text()').get()
+        desc = response.css("#product_description ~ p::text").get()
+        item["description"] = desc.strip() if desc else ""
 
-        yield {
-            "title": title,
-            "price": price,
-            "amount_in_stock": amount_in_stock,
-            "rating": rating,
-            "category": category,
-            "description": description,
-            "upc": upc,
-        }
+        item["upc"] = response.xpath(
+            '//th[text()="UPC"]/following-sibling::td/text()'
+        ).get(default="")
+
+        yield item
